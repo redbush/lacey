ed60baaa6c2e096d4f01abada8dbb666308de41c
Brian Rosedale <brian.j.rosedale@gmail.com>
2015-03-22 16:36:27 -0700

    add basic auth to page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
+  before_action :authenticate
+
   def home
+  end
+
+  private
+
+  def authenticate
+    return if admin_signed_in?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == SiteProperty.basic_auth_username && password == SiteProperty.basic_auth_password
+    end
   end
 end
