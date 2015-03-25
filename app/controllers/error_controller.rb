7f7d449c92a5b0b7b45d0a4ea85030690a69e09d
Brian Rosedale <brian.j.rosedale@gmail.com>
2015-03-24 18:11:00 -0700

    handle project not found

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -1,4 +1,15 @@
 class ErrorController < ApplicationController
+  before_action :authenticate
+
   def not_found
+  end
+
+  private
+
+  def authenticate
+    return if admin_signed_in?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == SiteProperty.basic_auth_username && password == SiteProperty.basic_auth_password
+    end
   end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,8 @@
 class ProjectsController < ApplicationController
+  before_action :authenticate
   before_action :set_project, only: [:show]
 
-  # TODO: authenticate
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def show
   end
@@ -10,5 +11,18 @@ class ProjectsController < ApplicationController
 
   def set_project
     @project = Project.find(params[:id])
+  end
+
+  def authenticate
+    return if admin_signed_in?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == SiteProperty.basic_auth_username && password == SiteProperty.basic_auth_password
+    end
+  end
+
+  private
+
+  def record_not_found
+    redirect_to not_found_path
   end
 end
